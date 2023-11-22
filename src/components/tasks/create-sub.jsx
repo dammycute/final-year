9544606b0e9c7664ab95042f13c72945cf781173
Damilola Olawoore <damilolaolawoore03@gmail.com>
2023-11-22 15:42:24 +0100

    gfg

--- src/components/tasks/create-sub.jsx
+++ src/components/tasks/create-sub.jsx
@@ -13,6 +13,42 @@ import UserSelect from "./create-combox";
 // import { ComboboxDemo } from "./create-combox";
 
 const CreateSub = () => {
+
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    priority: '',
+    owner: '',
+    startDate: '',
+    endDate: '',
+    projectId: '',
+    type: '',
+    labor: 0,
+    materials: 0,
+    otherExpenses: 0,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Assuming you have a backend API endpoint for creating a task
+      const response = await axios.post('/task/create', formData);
+      console.log('Task created successfully:', response.data);
+      // You can handle success, e.g., redirect to another page or display a success message
+    } catch (error) {
+      console.error('Error creating task:', error.response.data);
+      // You can handle errors, e.g., display an error message to the user
+    }
+  };
   return (
     <div>
       <p className="text-gray-400 font-xl my-4">Tasks / Create Task</p>
@@ -25,42 +61,42 @@ const CreateSub = () => {
                 type="text"
                 id="title"
                 name="title"
-                //   value={formData.title}
+                  value={formData.title}
                 className="mt-1 px-3 py-2 bg-white rounded-md border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                //   onChange={handleInputChange}
+                  onChange={handleInputChange}
               />
             </div>
             <div className="input w-2/6">
               <label htmlFor="title">Task Type</label>
               <input
                 type="text"
-                id="title"
-                name="title"
-                //   value={formData.title}
+                id="type"
+                name="type"
+                  value={formData.type}
                 className="mt-1 px-3 py-2 bg-white rounded-md border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                //   onChange={handleInputChange}
+                  onChange={handleInputChange}
               />
             </div>
             <div className="input w-1/6">
               <label htmlFor="title">Start Date</label>
               <input
                 type="date"
-                id="title"
-                name="title"
-                //   value={formData.title}
+                id="startDate"
+                name="startDate"
+                  value={formData.startDate}
                 className="mt-1 px-3 py-2 bg-white rounded-md border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                //   onChange={handleInputChange}
+                  onChange={handleInputChange}
               />
             </div>
             <div className="input w-1/6">
               <label htmlFor="title">End Date</label>
               <input
                 type="date"
-                id="title"
-                name="title"
-                //   value={formData.title}
+                id="endDate"
+                name="endDate"
+                  value={formData.endDate}
                 className="mt-1 px-3 py-2 bg-white rounded-md border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                //   onChange={handleInputChange}
+                  onChange={handleInputChange}
               />
             </div>
           </div>
@@ -69,44 +105,44 @@ const CreateSub = () => {
             <textarea
               type="textarea"
               rows={3}
-              id="title"
-              name="title"
-              //   value={formData.title}
+              id="description"
+              name="description"
+                value={formData.description}
               className="mt-1 px-3 py-2 bg-white rounded-md border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              //   onChange={handleInputChange}
+                onChange={handleInputChange}
             />
           </div>
 
           <div className="high flex gap-6">
             <div className="select">
-            <Label>Set Priority</Label>
-            <Select className="mt-2">
-              <SelectTrigger className="w-[120px]">
-                <SelectValue placeholder="High" value="High"/>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="high">High</SelectItem>
-                <SelectItem value="low">Low</SelectItem>
-                {/* <SelectItem value="system">System</SelectItem> */}
-              </SelectContent>
-            </Select>
+              <Label>Set Priority</Label>
+              <Select className="mt-2">
+                <SelectTrigger className="w-[120px]">
+                  <SelectValue placeholder="High" value="High" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="high">High</SelectItem>
+                  <SelectItem value="low">Low</SelectItem>
+                  {/* <SelectItem value="system">System</SelectItem> */}
+                </SelectContent>
+              </Select>
             </div>
             <div className="input">
               <label htmlFor="title">Cost</label>
               <input
-                type="text"
-                id="title"
-                name="title"
-                //   value={formData.title}
+                type="number"
+                id="cost"
+                name="cost"
+                  value={formData.cost}
                 className="mt-1 px-3 py-2 bg-white rounded-md border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                //   onChange={handleInputChange}
+                  onChange={handleInputChange}
               />
             </div>
           </div>
 
           <div className="buttom">
-          <div className="select">
-            <UserSelect backendUrl="https://jsonplaceholder.typicode.com/users"/>
+            <div className="select">
+              <UserSelect backendUrl="https://jsonplaceholder.typicode.com/users" />
             </div>
           </div>
 
---
+++ src/components/tasks/popover.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import PropTypes from "prop-types";
+import avatar from "../../../assets/images/avatar.png";
+import date from "../../../assets/images/calendar.svg";
+import clock from "../../../assets/images/clock.svg";
+import x from "../../assets/images/x.png";
+import assignee from "../../assets/images/assignee.png";
+import owner from "../../assets/images/owner.png";
+import status from "../../assets/images/status.png";
+import priority from "../../assets/images/priority.png";
+import plus from "../../assets/images/plus.png";
+import attach from "../../assets/images/Attach.png";
+
+import CountdownTimer from "../utils/time";
+// import x from "../../assets/images/x.png";
+// import x from "../../assets/images/x.png";
+
+const PopoverCard = ({ onClose }) => {
+  const handleFinish = () => {
+    // Logic to execute when the countdown finishes
+    console.log("Task completed!");
+  };
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <div className="popover-overlay container" onClick={handleClose}>
+      <div
+        className="popover-card container"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="popover-header">
+          <div className="pop-top flex justify-between">
+            <p className="text-gray-400">Project / Task ID-1234</p>
+            <img src={x} alt="" onClick={handleClose} />
+          </div>
+          <div className="flex title justify-between items-center py-4">
+            <h1 className="">Make a Suitable form</h1>
+            <div className="time badged1">
+              <img src={clock} alt="" />
+              <CountdownTimer
+                className="px-4"
+                initialHours={2}
+                onFinish={handleFinish}
+              />
+            </div>
+          </div>
+          <div className="desc-ctn">
+            <div className="assign">
+              <div className="left">
+                <img src={priority} alt="" />
+                <span>Priority</span>
+              </div>
+              <div className="right">
+                <button>High</button>
+              </div>
+            </div>
+          </div>
+          <div className="desc-ctn">
+            <div className="assign">
+              <div className="left">
+                <img src={status} alt="" />
+                <span>Status</span>
+              </div>
+              <div className="right green">
+                <button className="green">Completed</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="desc-ctn">
+            <div className="assign">
+              <div className="left">
+                <img src={owner} alt="" />
+                <span>Owner</span>
+              </div>
+              <div className="right people">
+                <img src={avatar} alt="" />
+                <span>UI Sharks</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="desc-ctn">
+            <div className="assign">
+              <div className="left">
+                <img src={assignee} alt="" />
+                <span>Assignee</span>
+              </div>
+              <div className="right people">
+                <img src={avatar} alt="" />
+                <span>Coder bhai</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="desc-ctn">
+            <div className="assign">
+              <div className="left">
+                <img src={date} alt="" />
+                <span>Due Date</span>
+              </div>
+              <div className="right">
+                <span>March 24th 2024</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="popover-content">
+          {/* Add content specific to the popover card */}
+          <div className="attachment">
+            <h2 className="font-bold">Attachments</h2>
+            <div className="document">
+              <img src={attach} alt="" />
+              <p>
+                <a href="">Document Links</a>
+              </p>
+            </div>
+            <div className="document">
+              <img src={plus} alt="" />
+              <p>Add Attachment</p>
+            </div>
+          </div>
+
+          <div className="attachment">
+            <h2 className="font-bold">Description</h2>
+            <div className="">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi natus tempora cupiditate aliquid maiores totam consequuntur eligendi laboriosam aperiam!</p>
+            </div>
+          </div>
+          <div className="popover-footer">
+            <img src={avatar} alt="" />
+            <p>Add Attachment</p>
+        </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+PopoverCard.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
+
+export default PopoverCard;
