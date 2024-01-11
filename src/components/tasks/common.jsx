09ceb26fd851f9db19c2edc6959dd9fcc09522a6
Damilola Olawoore <damilolaolawoore03@gmail.com>
2024-01-11 08:27:01 +0100

    The task title fix

--- src/components/tasks/common.jsx
+++ src/components/tasks/common.jsx
@@ -1,6 +1,9 @@
+import React, { useState, useEffect } from 'react';
 import clock from "../../../assets/images/clock.svg";
 // import DaysCounter from "../utils/days-count";
-import React from 'react';
+// import React from 'react';
+import axios from 'axios';
+import { useParams } from "react-router-dom";
 import DaysCounter from "../utils/months-count";
 
 const CommonComponent = () => {
@@ -10,11 +13,42 @@ const CommonComponent = () => {
     console.log("Task completed!");
   };
 
+  const { projectId } = useParams();
+  const userId = localStorage.getItem("user_id");
+  const [projectData, setProjectData] = useState(null);
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const fetchProjectData = async () => {
+      try {
+        const response = await axios.get(
+          `https://pm-api.cyclic.app/project/${userId}/projects-and-tasks`,
+          {
+            headers: {
+              Authorization: token,
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+        const data = response.data;
+        console.log("Project Data:", data);
+        setProjectData(data);
+      } catch (error) {
+        console.error("Error fetching project data:", error);
+      }
+    };
+
+    fetchProjectData();
+  }, [userId]);
+
+  // Extract projects array from projectData
+  const project = projectData?.projects.find(p => p._id === projectId);
+
   return (
     <div className="py-4 pl-8 pr-16">
       <div className="top-bar flex justify-between items-center">
         <div className="top-left">
-          <h2 className="font-bold">Slumbeigil</h2>
+          <h2 className="font-bold">{project?.title}</h2>
           <div className="top-left-text flex gap-4 items-center">
             <p className="text-[#9A93B3]">
               Edit or modify all cards as you want
@@ -38,7 +72,7 @@ const CommonComponent = () => {
             <div className="time badged1">
               <img src={clock} alt="" />
               <DaysCounter
-                days={6}
+                days={project.duration}
                 onFinish={handleFinish}
               />
             </div>
